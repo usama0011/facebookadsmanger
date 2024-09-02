ab2a59586bda706a8c249525bcd14bd73e4e6e10
unknown <za5232208@gmail.com>
2024-09-02 09:44:22 +0500

    usama done fas

--- src/components/CompaingsData.jsx
+++ src/components/CompaingsData.jsx
@@ -132,7 +132,8 @@ const CompaingsData = ({ campaigns, loading, error }) => {
       dataIndex: "campaingname",
       key: "campaingname",
       fixed: "left",
-      width: 200,
+      // here i want changne if showPerformanceAndClicks equals to performance then 200 otherwise 300
+      width: showPerformanceAndClicks === "performance" ? 200 : 300,
       render: (text, record) => (
         <>
           <div className="mainparentcontainer">
@@ -1318,7 +1319,7 @@ const CompaingsData = ({ campaigns, loading, error }) => {
                 <div>
                   <div class="x6s0dn4 x78zum5 x1q0g3np xozqiw3 x2lwn1j xeuugli x1iyjqo2 x65s2av">
                     <label
-                      style={{ marginBottom: "10px" }}
+                      style={{ marginBottom: "10px", cursor: "pointer" }}
                       id="js_14i"
                       for="js_14h"
                     >
@@ -1331,6 +1332,7 @@ const CompaingsData = ({ campaigns, loading, error }) => {
                     style={{
                       padding: "10px 20px",
                       borderRadius: "10px",
+                      cursor: "pointer",
                       backgroundColor:
                         showPerformanceAndClicks === "performance"
                           ? "#ecf2fe"
@@ -1417,6 +1419,7 @@ const CompaingsData = ({ campaigns, loading, error }) => {
                           : "", // Corrected ternary and removed double curly braces
                       padding: "10px 20px",
                       borderRadius: "10px",
+                      cursor: "pointer",
                     }}
                     onClick={handleCloseBannerPerformanceandClicks}
                     class="x1i10hfl x1qjc9v5 xjbqb8w xjqpnuy xa49m3k xqeqjp1 x2hbi6w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x78zum5 xdl72j9 xdt5ytf x2lah0s xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x16tdsg8 x1hl2dhg xggy1nq x1ja2u2z x1h6gzvc x1t137rt"
